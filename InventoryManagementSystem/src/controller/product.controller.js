3f4d2928615e18f6d4734778f191e2585c832718
unknown <priyankaghorpade1947@gmail.com>
2024-09-27 10:58:10 +0530

    add changes

--- InventoryManagementSystem/src/controller/product.controller.js
+++ InventoryManagementSystem/src/controller/product.controller.js
@@ -1,17 +1,19 @@
 import path from "path";
-import productModel from "../model/model.js";
+import productModel from "../model/product.model.js";
 
 export default class projectController{
     getProduct(req,res) {
         console.log('inside getProduct');        
         let products = productModel.getAll();
-        return res.render('index',{products}); 
+        return res.render('index',{products,
+            userEmail:req.session.userEmail}); 
         // return res.sendFile(path.join(path.resolve(),'src','view','index.html'));        
     }
 
     getAddForm(req,res){
         console.log('inside getAddForm');
-        return res.render('new-product',{errormessage:null});
+        return res.render('new-product',{errormessage:null,
+            userEmail:req.session.userEmail});
     }
     addNewProduct(req, res) {
         console.log('inside addNewProduct');        
@@ -25,7 +27,8 @@ export default class projectController{
         let products = productModel.getAll();
 
         // again render the products page
-        return res.render('index',{products});
+        return res.render('index',{products,
+            userEmail:req.session.userEmail});
         
 
 
@@ -35,20 +38,23 @@ export default class projectController{
         const id = req.params.id;
 
         const productFound = productModel.getByID(id);
-        return res.render('update-product',{errormessage:null, products:productFound});
+        return res.render('update-product',{errormessage:null, products:productFound,
+            userEmail:req.session.userEmail});
     }
 
     postUpdateProduct(req,res){
         productModel.update(req.body);
         let products = productModel.getAll();
-        return res.render('index',{products});
+        return res.render('index',{products,
+            userEmail:req.session.userEmail});
     }
     deleteProduct(req,res){
         const id = req.params.id;
         productModel.delete(id);
        
         let products = productModel.getAll();
-        return res.render('index',{products})
+        return res.render('index',{products,
+            userEmail:req.session.userEmail})
 
     }
     logout(req, res) {
